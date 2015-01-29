afe68d24b65d50d5c756952c0474471e75c0f8a3
nielsenengrade <nnielsen@engrade.com>
2015-01-28 16:49:52 -0800

    Protocol super user routes

---
+++ app/routes/partials/restricted/_protocol.php
@@ -0,0 +1,11 @@
+<?php
+
+Route::group(['prefix'=>'_protocol'],function() {
+
+	Route::get('invite-all-users',function() {
+		foreach(Subscriber::current()->users()->where('password','=',NULL)->get() as $user) {
+			(new UserController)->newAccountInvitation($user);
+		}
+	});
+
+});
