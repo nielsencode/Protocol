2d8e3be7cd50bdd679f9d0682a4ae7a5a1675276
nicknielsencode <nick@nicknielsencode.com>
2015-03-25 11:53:03 -0700

    Edit a user's role

--- app/composers/Users/AddComposer.php
+++ app/composers/Users/AddComposer.php
@@ -3,21 +3,34 @@
 class AddComposer {
 
 	public function compose($view) {
-		switch(\Auth::user()->role->name) {
-			case 'protocol':
-				$roleNames = array('subscriber','admin','client');
-				break;
-			case 'subscriber':
-				$roleNames = array('admin','client');
-				break;
+
+		$user = \Auth::user();
+
+		if (
+			$user
+				->has('add')
+				->ofScope('Subscriber',\Subscriber::current()->id)
+				->orScope('Protocol')
+				->over('User')
+		) {
+
+			switch ($user->role->name) {
+				case 'protocol':
+					$roleNames = array('subscriber', 'admin');
+					break;
+				case 'subscriber':
+					$roleNames = array('admin');
+					break;
+			}
+
+			$roles = \Role::whereIn('name', $roleNames)->lists('name', 'id');
+
+			$data = array(
+				'roles' => $roles
+			);
+
+			$view->with($data);
+
 		}
-
-		$roles = \Role::whereIn('name',$roleNames)->lists('name','id');
-
-		$data = array(
-			'roles'=>$roles
-		);
-
-		$view->with($data);
 	}
 }
